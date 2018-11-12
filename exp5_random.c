51cc56c8d729edba87bdfc4015939acb24bbc283
haoye999 <hanlianhao1234@163.com>
2018-11-12 15:18:27 +0800

    exp5_final

--- exp5_random.c
+++ exp5_random.c
@@ -22,12 +22,11 @@ void get_random(int a[rMAX][cMAX])
 
 void show_random(int a[rMAX][cMAX])
 {
-    printf("Random array:\n");
     for (int i = 0; i < rMAX; i++)
     {
         for (int j = 0; j < cMAX; j++)
         {
-            printf("%d\t", a[i][j]);
+            printf("%2d\t", a[i][j]);
         }
         printf("\n");
     }
@@ -42,14 +41,34 @@ void exchange_row(int a[rMAX][cMAX], int x, int y)
     memcpy(a[y], temp, length);
 }
 
+void show_max(int a[rMAX][cMAX])
+{
+    for (int i = 0; i < rMAX; i++)
+    {
+        int nmax = 0;
+        int colnum_max = 0;
+        for (int j = 0; j < cMAX; j++)
+        {
+            if (a[i][j] > nmax)
+            {
+                nmax = a[i][j];
+                colnum_max = j;
+            }
+        }
+        printf("The max number of this row %d is %d in col %d\n", i + 1, nmax, colnum_max + 1);
+    }
+}
+
 int main(void)
 {
     system("clear");
     srand((unsigned int)time(NULL));
     get_random(a);
+    printf("Random array:\n");
     show_random(a);
     exchange_row(a, 0, 4);
+    printf("Exchange row 1 with row 5. \nRandom array:\n");
     show_random(a);
-
+    show_max(a);
     return 0;
 }
